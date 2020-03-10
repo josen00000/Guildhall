68f1623e004a07994af9230b7269bb2cea32a764
48101070 <josenz@smu.edu>
2020-03-10 14:00:32 -0500

    working on tes

--- Engine/Code/Engine/Renderer/MeshUtils.cpp
+++ Engine/Code/Engine/Renderer/MeshUtils.cpp
@@ -313,9 +313,9 @@ void AppendTesselateIndexedVerts( std::vector<Vertex_PCU>& dest, std::vector<uin
 		Vertex_PCU vertCA = Vertex_PCU::GetMiddleVertForTwoVerts( vertC, vertA );
 
 		// triangle A, AB, AC
-		vertices.push_back( vertA );
-		vertices.push_back( vertAB );
-		vertices.push_back( vertCA );
+		//vertices.push_back( vertA );
+		//vertices.push_back( vertAB );
+		//vertices.push_back( vertCA );
 
 		//triangle AB, B, BC
 		vertices.push_back( vertAB );
